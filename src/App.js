3dc6099f2ea16eb421c74d7c98d2c93cc1d75b6f
A-Doubt <adoubt2@gmail.com>
2022-03-30 20:37:02 +0200

    Refactored state, added gameOver state, added card shuffling.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
 import React from 'react';
 import Card from './components/Card';
-import { allNewCards, createCard } from './cardsHelpers';
+// eslint-disable-next-line no-unused-vars
+import { allNewCards, createCard, shuffleArray } from './cardsHelpers';
 
 function App() {
 
-  const [cardsClicked, setCardsClicked] = React.useState([])
-  const [cards, setCards] = React.useState(allNewCards())
+	const [cards, setCards] = React.useState(allNewCards())
+	const [gameOver, setGameOver] = React.useState(false)
 
-  const cardsElements = cards.map((card) => {
-    return <Card 
-      value={card.name}  
-      key={card.id} 
-      handleClick={() => markCard(card.name)}
-    />
+	const cardsElements = cards.map((card) => {
+		return <Card 
+		value={card.name}  
+		key={card.id} 
+		handleClick={() => markCard(card.name)}
+		/>
+		
+	})
 
-  })
+	function markCard(value) {
+		console.log('markCard')
+		console.log(gameOver)
+		cards.forEach((card) => {
+			if (card.clicked && value === card.name) {
+				setGameOver(true)
+			}
+		})
 
-  function markCard(value) {
-    setCardsClicked((prevState) => {
-      return [...prevState, value]
-    })
-  }
+		if (gameOver) return
+		setCards((prevState) => {
+			const newArray = prevState.map((card) => {
+				return card.name === value ? {...card, clicked: true} : card
+			})
+			shuffleArray(newArray)
+			return newArray
+		})
 
-  React.useEffect(() => {
-    console.log(cardsClicked)
-  }, [cardsClicked])
+	}
+	
+	React.useEffect(() => {
+		console.log('from useEffect ', gameOver)
+	}, [gameOver])
 
-
-  
-  return (
-    <div className='App'>
-      <main>
-        <h1>Memory game</h1>
-        <p>Some random text</p>
-        <div className='cards'>
-          {cardsElements}
-        </div>
-      </main>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<main>
+				<h1>Memory game</h1>
+				<p>Some random text</p>
+				<div className='cards'>
+				{cardsElements}
+				</div>
+			</main>
+		</div>
+	);
 }
-
+	
 export default App;
+	
--- src/cardsHelpers.js
+++ src/cardsHelpers.js
@@ -1,19 +1,35 @@
 import { nanoid } from 'nanoid';
 
 export function createCard(name, id) {
-    return {
-      name: name,
-      id: id
-    }
-  }
+	return {
+		name: name,
+		id: id,
+		clicked: false
+	}
+}
 
 export function allNewCards() {
-    const cardsArray = []
-    cardsArray.push(createCard('Charmander', nanoid()));
-    cardsArray.push(createCard('Squirtle', nanoid()));
-    cardsArray.push(createCard('Ditto', nanoid()));
-    cardsArray.push(createCard('Onyx', nanoid()));
-    cardsArray.push(createCard('Lucario', nanoid()));
-    cardsArray.push(createCard('Zapdos', nanoid()));
-    return cardsArray;
-  };
+	const cardsArray = []
+	cardsArray.push(createCard('Charmander', nanoid()));
+	cardsArray.push(createCard('Squirtle', nanoid()));
+	cardsArray.push(createCard('Ditto', nanoid()));
+	cardsArray.push(createCard('Onyx', nanoid()));
+	cardsArray.push(createCard('Lucario', nanoid()));
+	cardsArray.push(createCard('Zapdos', nanoid()));
+	return cardsArray;
+};
+
+
+export function shuffleArray(array) {
+	for (var i = array.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+}
+
+let i = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(i)
+shuffleArray(i)
+console.log(i)

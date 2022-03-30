4464e17cfa3fc193dfe9d9b509ceff53c351e1e2
A-Doubt <adoubt2@gmail.com>
2022-03-30 22:37:46 +0200

    Functionality and styles
    Added the following:
    
    1. Replaced text with pokemon images
    2. Current score tracker
    3. pokemon fonts and logo
    4. ability to reset the game
    5. conditional rendering of cards when it's not game over

--- src/App.js
+++ src/App.js
@@ -5,50 +5,85 @@ import { allNewCards, createCard, shuffleArray } from './cardsHelpers';
 
 function App() {
 
-	const [cards, setCards] = React.useState(allNewCards())
-	const [gameOver, setGameOver] = React.useState(false)
+	const [cards, setCards] = React.useState(allNewCards());
+	const [gameOver, setGameOver] = React.useState(false);
+	const [currentScore, setCurrentScore] = React.useState(0);
+	const [bestScore, setBestScore] = React.useState(0)
 
 	const cardsElements = cards.map((card) => {
 		return <Card 
-		value={card.name}  
+		value={card.number}  
 		key={card.id} 
-		handleClick={() => markCard(card.name)}
+		handleClick={() => markCard(card.number)}
 		/>
-		
 	})
 
 	function markCard(value) {
-		console.log('markCard')
-		console.log(gameOver)
+		console.log('from markCard', gameOver)
 		cards.forEach((card) => {
-			if (card.clicked && value === card.name) {
-				setGameOver(true)
+			if (card.clicked && value === card.number) {
+				setGameOver(true);
 			}
 		})
 
-		if (gameOver) return
 		setCards((prevState) => {
 			const newArray = prevState.map((card) => {
-				return card.name === value ? {...card, clicked: true} : card
+				return card.number === value ? {...card, clicked: true} : card
 			})
 			shuffleArray(newArray)
 			return newArray
 		})
 
+		setCurrentScore((prevScore) => {
+			return prevScore + 1;
+		})
 	}
-	
+
 	React.useEffect(() => {
-		console.log('from useEffect ', gameOver)
+		// game reset will go here
+		setCurrentScore((prevScore) => {
+			if (prevScore === 0) return 0
+			return prevScore - 1;
+		})
+
+		console.log('from useEffect gameOver ', gameOver)
 	}, [gameOver])
+
+	React.useEffect(() => {
+		console.log('from useEffect, best score ', bestScore)
+
+	}, [currentScore])
+
+
+	function resetGame() {
+		setCards(allNewCards());
+		setCurrentScore(0);
+		setGameOver(false);
+	}
 
 	return (
 		<div className='App'>
 			<main>
-				<h1>Memory game</h1>
-				<p>Some random text</p>
-				<div className='cards'>
-				{cardsElements}
+				<div className='title'>
+					{/* <h1 className='title--main'>Memory game</h1> */}
 				</div>
+				<img src={require('./assets/logo-bent.png')} alt='pokemon'/>
+				<p className='current-score'>
+					Current score: {currentScore}
+				</p>
+				<p className='best-score'>
+					Best score: {bestScore}
+				</p>
+				<div className='cards'>
+				{!gameOver && cardsElements}
+				</div>
+				{gameOver && 
+					<button 
+						className='reset-btn'
+						onClick={resetGame}
+					>
+							Try again!
+					</button>}
 			</main>
 		</div>
 	);
--- src/cardsHelpers.js
+++ src/cardsHelpers.js
@@ -1,8 +1,9 @@
 import { nanoid } from 'nanoid';
 
-export function createCard(name, id) {
+export function createCard(name, number, id) {
 	return {
 		name: name,
+		number: number,
 		id: id,
 		clicked: false
 	}
@@ -10,12 +11,27 @@ export function createCard(name, id) {
 
 export function allNewCards() {
 	const cardsArray = []
-	cardsArray.push(createCard('Charmander', nanoid()));
-	cardsArray.push(createCard('Squirtle', nanoid()));
-	cardsArray.push(createCard('Ditto', nanoid()));
-	cardsArray.push(createCard('Onyx', nanoid()));
-	cardsArray.push(createCard('Lucario', nanoid()));
-	cardsArray.push(createCard('Zapdos', nanoid()));
+	cardsArray.push(createCard('Charizard', '0006', nanoid()));
+	cardsArray.push(createCard('Blastoise', '0009', nanoid()));
+	cardsArray.push(createCard('Metapod', '0011', nanoid()));
+	cardsArray.push(createCard('Ekans', '0023', nanoid()));
+	cardsArray.push(createCard('Pikachu', '0025',nanoid()));
+	cardsArray.push(createCard('Sandslash', '0028', nanoid()));
+	cardsArray.push(createCard('Nidoking', '0034', nanoid()));
+	cardsArray.push(createCard('Psyduck', '0054', nanoid()));
+	cardsArray.push(createCard('Poliwag', '0060', nanoid()));
+	cardsArray.push(createCard('Tentacool', '0072', nanoid()));
+	cardsArray.push(createCard('Slowpoke', '0079',nanoid()));
+	cardsArray.push(createCard('Dewgong', '0087', nanoid()));
+	cardsArray.push(createCard('Haunter', '0093', nanoid()));
+	cardsArray.push(createCard('Hypno', '0097', nanoid()));
+	cardsArray.push(createCard('Scyther', '0123', nanoid()));
+	cardsArray.push(createCard('Ditto', '0132', nanoid()));
+	cardsArray.push(createCard('Kabutops', '0141', nanoid()));
+	cardsArray.push(createCard('Articuno', '0144',nanoid()));
+	cardsArray.push(createCard('Zapdos', '0145', nanoid()));
+	cardsArray.push(createCard('Moltres', '0146', nanoid()));
+
 	return cardsArray;
 };
 
@@ -28,8 +44,3 @@ export function shuffleArray(array) {
 		array[j] = temp;
 	}
 }
-
-let i = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(i)
-shuffleArray(i)
-console.log(i)
